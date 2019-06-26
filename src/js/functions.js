653fbc147d41c9a3e101cc4c3bca4e6c9e0f8a8e
abir <abirchandrabhushan@gmail.com>
2019-06-26 22:43:44 +0530

    Minor code change

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    var sections = document.getElementsByClassName('rad_section');
+  var sections = document.getElementsByClassName('rad_section');
 
-    for (var i = sections.length; i--;) {
-        sections[i].addEventListener('change', function() {
-            storeSelection(this.id);
-        }, false);
-    }
+  for (var i = sections.length; i--;) {
+    sections[i].addEventListener('change', function() {
+      storeSelection(this.id);
+    }, false);
+  }
 
-    var selection = localStorage.latexcheatsheet == undefined ? 'ac-head' : localStorage.latexcheatsheet;
-    document.getElementById(selection)
-        .checked = true;
+  var selection = localStorage.latexcheatsheet == undefined ? 'ac-head' : localStorage.latexcheatsheet;
+  document.getElementById(selection)
+    .checked = true;
 });
 
 function storeSelection(id) {
-    localStorage.latexcheatsheet = id;
+  localStorage.latexcheatsheet = id;
 }
